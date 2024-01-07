a119006f19f6eb67e4f96aa8715d3bbadc1c3d2a
Michael <minavat@microsoft.com>
2024-01-07 12:12:02 +0200

    Basically works

---
+++ lm.py
@@ -0,0 +1,79 @@
+import hashlib
+import importlib.util
+import os
+import re
+import sys
+
+from openai import OpenAI
+
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+PYTHON_EXTRACT_RE = re.compile('```python\n(.*?)```', re.MULTILINE | re.DOTALL)
+
+MODEL_GPT_3_5 = 'gpt-3.5-turbo'
+MODEL_GPT_4 = 'gpt-4-32k-0613'
+MODEL = MODEL_GPT_3_5
+
+
+def _get_cache_filename(prompt, suffix='txt'):
+    sha256 = hashlib.sha256(prompt.encode()).hexdigest()
+    return f'cache/{(prompt[:5] + " " + prompt[-5:]).strip().lower().replace(" ", "_")}_{sha256[:10]}_{MODEL}.{suffix}'
+
+
+def _read_cached_prompt_result(prompt):
+    filename = _get_cache_filename(prompt)
+    if os.path.isfile(filename):
+        return open(filename, 'r').read()
+    return None
+
+
+def send_prompt(prompt):
+    cached_result = _read_cached_prompt_result(prompt)
+    if cached_result is not None:
+        return cached_result
+
+    # Call the OpenAI API to generate a completion
+    completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model=MODEL,
+    )
+
+    # Extract and print the response_text
+    response_text = completion.choices[0].message.content.strip()
+
+    open(_get_cache_filename(prompt), 'w').write(response_text)
+
+    return response_text
+
+
+def extract_code_from_response(response):
+    regex_results = PYTHON_EXTRACT_RE.findall(response)
+    if len(regex_results) == 0:
+        return response
+    return regex_results[0]
+
+
+def prompt_and_load_code(prompt):
+    code = extract_code_from_response(
+        send_prompt(prompt)
+    )
+    filename = _get_cache_filename(prompt, 'py')
+    open(filename, 'w').write(code)
+    spec = importlib.util.spec_from_file_location("dynamic.code", filename)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules["dynamic.code"] = module
+    spec.loader.exec_module(module)
+
+    return module
+
+
+if __name__ == '__main__':
+    print(send_prompt('Generate hello world Python code'))
--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from Levenshtein import distance
 
-from llm import prompt_and_extract_code, send_prompt
+from lm import send_prompt, prompt_and_load_code
 
-DISTANCE_THRESHOLD = 3
-
-PROMPT_MASK_LIST_ENTITIES = 'Given the env vars HOME_ASSISTANT_URL and HOME_ASSISTANT_TOKEN a user wants to {command}. ' \
-                            'Write Python function "list_entities" that returns a list of all entities that can perform this action. '
+PROMPT_MASK_LIST_ENTITIES = 'Given the env vars HOME_ASSISTANT_URL and HOME_ASSISTANT_TOKEN a user wants to ' \
+                            '{command}. ' \
+                            'Write Python function "list_entities()" that returns a list of all entity objects ' \
+                            'that can perform this action. ' \
+                            'Consider only by entity prefix, entity_id and friendly name, but return the entire object. ' \
+                            'Don''t use supported_features. ' \
+                            'Not all entities has friendly_name. ' \
+                            'Import and use os, requests and other necessary packages. ' \
+                            'Do not call the function.'
 PROMPT_MASK_SELECT_ENTITIES = 'A user asked to {command}. ' \
                               'Which of the following entities is or are the best to perform this request?\n' \
                               '{entities}\n\n' \
                               'Provide the shortest answer possible.'
 PROMPT_DO_THE_COMMAND = 'Given the env vars HOME_ASSISTANT_URL and HOME_ASSISTANT_TOKEN a user wants to {command}. ' \
-                        'Write Python function call_entity() to do that to entity_id {entity_id} and string result.'
+                        'Write Python function "call_entity()" to do that to the entity_id {entity_id} and ' \
+                        'return string result. ' \
+                        'Import all necessary packages. ' \
+                        'Do not call the function.'
+
+DISTANCE_THRESHOLD = 3
 
 
 def filter_entities(entities_list, best_entity_answer):
@@ -19,17 +29,20 @@ def filter_entities(entities_list, best_entity_answer):
             distance(e['attributes']['friendly_name'], best_entity_answer) < DISTANCE_THRESHOLD]
 
 
-def main():
-    command = 'Lock the door'
-    prompt_and_extract_code(PROMPT_MASK_LIST_ENTITIES.format(command=command))
-    entities_list = [{'entity_id': 'input_text.main_door_unlock_code_typed', 'state': '322323231233223', 'attributes': {'editable': True, 'min': 0, 'max': 30, 'pattern': None, 'mode': 'password', 'icon': 'mdi:key', 'friendly_name': 'Main door unlock code typed'}, 'last_changed': '2024-01-04T21:04:37.663668+00:00', 'last_updated': '2024-01-04T21:04:37.663668+00:00', 'context': {'id': '01HKB4NN8XZ8NYKAJAZB9CTCS9', 'parent_id': '01HKB4NMR1XFWGKE0K2H2JN9EJ', 'user_id': None}}, {'entity_id': 'automation.door_lock_keypad_unlock', 'state': 'on', 'attributes': {'id': '1699946729725', 'last_triggered': '2024-01-04T21:04:37.662026+00:00', 'mode': 'single', 'current': 0, 'friendly_name': 'Door lock: keypad unlock'}, 'last_changed': '2024-01-04T07:16:31.179880+00:00', 'last_updated': '2024-01-04T21:04:37.664328+00:00', 'context': {'id': '01HKB4NN8XZ8NYKAJAZB9CTCS9', 'parent_id': '01HKB4NMR1XFWGKE0K2H2JN9EJ', 'user_id': None}}, {'entity_id': 'binary_sensor.nuki_main_locked', 'state': 'on', 'attributes': {'timestamp': '2024-01-05T06:00:30', 'device_class': 'lock', 'friendly_name': 'Nuki Main Locked'}, 'last_changed': '2024-01-05T05:43:21.122793+00:00', 'last_updated': '2024-01-05T06:00:29.547714+00:00', 'context': {'id': '01HKC3AVKBN04KGK7HKREWJSB8', 'parent_id': None, 'user_id': None}}, {'entity_id': 'lock.nuki_main_lock', 'state': 'unlocked', 'attributes': {'friendly_name': 'Nuki Main Lock', 'supported_features': 1}, 'last_changed': '2024-01-05T05:43:21.123408+00:00', 'last_updated': '2024-01-05T05:43:21.123408+00:00', 'context': {'id': '01HKC2BF93JVP22GC81RKDRQ2V', 'parent_id': None, 'user_id': None}}, {'entity_id': 'lock.kids_shower_heater_child_lock', 'state': 'unlocked', 'attributes': {'friendly_name': 'Kids shower heater Child lock', 'supported_features': 0}, 'last_changed': '2024-01-04T07:16:45.930835+00:00', 'last_updated': '2024-01-04T07:16:45.930835+00:00', 'context': {'id': '01HK9N9SQAB677TJA7K90E2W6K', 'parent_id': None, 'user_id': None}}, {'entity_id': 'lock.parents_shower_heater_child_lock', 'state': 'unlocked', 'attributes': {'friendly_name': 'Parents shower heater Child lock', 'supported_features': 0}, 'last_changed': '2024-01-04T07:18:14.880082+00:00', 'last_updated': '2024-01-04T07:18:14.880082+00:00', 'context': {'id': '01HK9NCGK071A1M0K8EQAGKZ4F', 'parent_id': None, 'user_id': None}}, {'entity_id': 'lock.router_plug_child_lock', 'state': 'unlocked', 'attributes': {'friendly_name': 'Router plug Child lock', 'supported_features': 0}, 'last_changed': '2024-01-04T07:18:14.881876+00:00', 'last_updated': '2024-01-04T07:18:14.881876+00:00', 'context': {'id': '01HK9NCGK1D6SJN78WAK55TY1V', 'parent_id': None, 'user_id': None}}, {'entity_id': 'switch.roborock_q_revo_child_lock', 'state': 'on', 'attributes': {'icon': 'mdi:account-lock', 'friendly_name': 'Roborock Q Revo Child lock'}, 'last_changed': '2024-01-04T07:17:01.232877+00:00', 'last_updated': '2024-01-04T07:17:01.232877+00:00', 'context': {'id': '01HK9NA8NGKHHJGEYB6K15PN8K', 'parent_id': None, 'user_id': None}}]
+def main(command):
+    module_list_entities = prompt_and_load_code(PROMPT_MASK_LIST_ENTITIES.format(command=command))
+    entities_list = module_list_entities.list_entities()
     entities_names = [e['attributes']['friendly_name'] for e in entities_list]
-    prompt_and_extract_code(PROMPT_MASK_SELECT_ENTITIES.format(command=command, entities=entities_names))
-    best_entity_answer = '"Nuki Main Lock"'
+
+    best_entity_answer = send_prompt(
+        PROMPT_MASK_SELECT_ENTITIES.format(command=command, entities=entities_names))
+
     best_entities = filter_entities(entities_list, best_entity_answer)
     best_entities_ids = [e['entity_id'] for e in best_entities]
-    print(send_prompt(PROMPT_DO_THE_COMMAND.format(command=command, entity_id=','.join(best_entities_ids))))
+    module_call_entity = prompt_and_load_code(PROMPT_DO_THE_COMMAND.format(command=command, entity_id=','.join(best_entities_ids)))
+
+    print(module_call_entity.call_entity())
 
 
 if __name__ == '__main__':
-    main()
+    main('Lock the door')
